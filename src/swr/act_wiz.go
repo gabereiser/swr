2ecacebf2293709ba59157ab3bb0cdc9f657e7c7
Gabriel Reiser <gabe@gabereiser.com>
2022-08-04 22:18:11 -0400

    Fix bug with rexit and aset

--- src/swr/act_wiz.go
+++ src/swr/act_wiz.go
@@ -29,6 +29,9 @@ func do_area_create(entity Entity, args ...string) {
 	}
 	if len(args) < 3 {
 		entity.Send("\r\nSyntax: acreate <areaname> <min vnum> <max vnum>\r\n")
+		entity.Send("-----------------------------------------------------------\r\n")
+		entity.Send("*NOTE* area create will create up to max vnum, but not including.\r\n")
+		entity.Send("Using 100 200 will create 99 rooms starting at 100.\r\n")
 		return
 	}
 	min_vnum, err := strconv.ParseInt(args[1], 10, 32)
@@ -55,14 +58,20 @@ func do_area_create(entity Entity, args ...string) {
 	area.ResetMsg = "The world seems to shift around you."
 	for i := min_vnum; i < max_vnum; i++ {
 		room := RoomData{
-			Id:   uint(i),
-			Name: "A void",
-			Desc: "Somewhere in the void of space.",
+			Id:        uint(i),
+			Name:      "A void",
+			Desc:      "Somewhere in the void of space.",
+			Flags:     make([]string, 0),
+			Items:     make([]Item, 0),
+			Exits:     make(map[string]uint),
+			ExitFlags: make(map[string]*RoomExitFlag),
+			Area:      area,
+			RoomProgs: make(map[string]string),
 		}
 		area.Rooms = append(area.Rooms, room)
 	}
 	db.SaveArea(area)
-	entity.Send("\r\n&GDone&d\r\n")
+	entity.Send("\r\n&YArea Create. Ok.&d\r\n")
 }
 
 func do_area_set(entity Entity, args ...string) {
@@ -101,19 +110,20 @@ func do_area_set(entity Entity, args ...string) {
 		case "reset":
 			r, _ := strconv.Atoi(args[1])
 			room.Area.Reset = uint(r)
-		case "resetMsg":
+		case "resetmsg":
 			room.Area.ResetMsg = strings.TrimSpace(strings.Join(args[1:], " "))
 		default:
 			entity.Send("\r\n&RInvalid field.&d\r\n")
 		}
 	}
+	entity.Send("\r\n&YArea Set. Ok&d\r\n")
 }
 
 func do_area_remove(entity Entity, args ...string) {
 	for i, area := range DB().areas {
 		if strings.EqualFold(area.Name, args[0]) {
 			DB().RemoveArea(DB().areas[i])
-			entity.Send("\r\n&GArea Removed.&d\r\n")
+			entity.Send("\r\n&YArea Remove. Ok.&d\r\n")
 			return
 		}
 	}
@@ -127,7 +137,7 @@ func do_area_reset(entity Entity, args ...string) {
 		for i, area := range DB().areas {
 			if strings.EqualFold(area.Name, args[0]) {
 				area_reset(DB().areas[i])
-				entity.Send("\r\n&YReset. Ok.&d\r\n")
+				entity.Send("\r\n&YArea Reset. Ok.&d\r\n")
 				return
 			}
 		}
@@ -229,32 +239,54 @@ func do_room_make_exit(entity Entity, args ...string) {
 		log.Printf("FATAL: Unable to determine room for roomId %d and locationId %d", vnum, entity.ShipId())
 		return
 	}
-	if room.Area != nil {
-		if to_room.Area != nil {
-			if to_room.Area.Name != room.Area.Name {
-				entity.Send("\r\n&RFATAL! Rooms are not in the same area!!!&d\r\n")
-				return
-			}
-		}
-	} else if room.ship > 0 {
-		if to_room.ship != room.ship {
-			entity.Send("\r\n&RFATAL! Rooms are not in the same area!!!&d\r\n")
-			return
-		}
+	if room.ship != to_room.ship {
+		entity.Send("\r\n&RFATAL! Rooms are not in the same area!!!&d\r\n")
+		return
 	}
 	room.Exits[dir] = to_room.Id
 	entity.Send("\r\n&YExit. Ok.&d\r\n")
 }
 
-func do_room_edit(entity Entity, args ...string) {
-
-}
-
-func do_room_reset(entity Entity, args ...string) {
-
-}
-
 func do_room_remove(entity Entity, args ...string) {
+	if entity == nil {
+		return
+	}
+	if len(args) != 1 {
+		entity.Send("\r\nSyntax: rremove <vnum>\r\n")
+		entity.Send("-----------------------------------------------------------------\r\n")
+		entity.Send("*NOTE* rremove will reset a room back to it's prototype state.\r\n")
+		entity.Send("Use with caution.\r\n")
+		entity.Send("Will reset a room in your current area/ship.\r\n")
+		return
+	}
+	vnum, _ := strconv.Atoi(args[0])
+	room := DB().GetRoom(uint(vnum), entity.ShipId())
+	if room == nil {
+		entity.Send("\r\n&RRoom not found in your area.&d\r\n")
+		return
+	}
+	for dir, rId := range room.Exits {
+		eroom := DB().GetRoom(rId, entity.ShipId())
+		delete(eroom.Exits, direction_reverse(dir))
+	}
+	room.Name = "A void"
+	room.Desc = "Somewhere in the void of space."
+	room.Exits = make(map[string]uint)
+	room.ExitFlags = make(map[string]*RoomExitFlag)
+	room.Flags = make([]string, 0)
+	room.Items = make([]Item, 0)
+	room.RoomProgs = make(map[string]string)
+	if room.ship > 0 {
+		ship := DB().GetShip(room.ship)
+		ship.GetData().Rooms[room.Id] = room
+	} else {
+		for i, r := range room.Area.Rooms {
+			if r.Id == room.Id {
+				room.Area.Rooms[i] = *room
+			}
+		}
+	}
+	entity.Send("\r\n&YRemove. Ok.&d\r\n")
 
 }
 
