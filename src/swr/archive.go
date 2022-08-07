b406d25033db94c9ec67f585d07a576370761d5a
Gabriel Reiser <gabe@gabereiser.com>
2022-08-07 17:23:55 -0400

    Fixing backup logging

--- src/swr/archive.go
+++ src/swr/archive.go
@@ -111,20 +111,14 @@ func create_backup(t time.Time) {
 	if err != nil {
 		log.Fatalln("Error creating archive:", err)
 	}
-
-	fmt.Println("Archive created successfully")
+	log.Println("Backup archive created successfully")
 }
 func create_archive(files []string, buf io.Writer) error {
-	// Create new Writers for gzip and tar
-	// These writers are chained. Writing to the tar writer will
-	// write to the gzip writer which in turn will write to
-	// the "buf" writer
 	gw := gzip.NewWriter(buf)
 	defer gw.Close()
 	tw := tar.NewWriter(gw)
 	defer tw.Close()
 
-	// Iterate over files and add them to the tar archive
 	for _, file := range files {
 		err := archive_add(tw, file)
 		if err != nil {
@@ -136,38 +130,28 @@ func create_archive(files []string, buf io.Writer) error {
 }
 
 func archive_add(tw *tar.Writer, filename string) error {
-	// Open the file which will be written into the archive
 	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	// Get FileInfo about our file providing file size, mode, etc.
 	info, err := file.Stat()
 	if err != nil {
 		return err
 	}
 
-	// Create a tar Header from the FileInfo data
 	header, err := tar.FileInfoHeader(info, info.Name())
 	if err != nil {
 		return err
 	}
-
-	// Use full path as name (FileInfoHeader only takes the basename)
-	// If we don't do this the directory strucuture would
-	// not be preserved
-	// https://golang.org/src/archive/tar/common.go?#L626
 	header.Name = filename
 
-	// Write file header to the tar archive
 	err = tw.WriteHeader(header)
 	if err != nil {
 		return err
 	}
 
-	// Copy file content to tar archive
 	_, err = io.Copy(tw, file)
 	if err != nil {
 		return err
